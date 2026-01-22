6637a007f2f3b255da7b4093361209d054b6d861
fardin ahsan <fardin.ahsan75@gmail.com>
2026-01-23 02:33:05 +0600

    app config

--- config/admin.php
+++ config/admin.php
@@ -5,6 +5,7 @@ return [
      * If set, this user becomes Super Admin and bypasses all role/permission checks.
      * If empty/null, the first user (id=1) is treated as Super Admin.
      */
-    'super_admin_email' => env('SUPER_ADMIN_EMAIL', 'admin@admin.com'),
+    'super_admin_email' => env('APP_SUPER_ADMIN_EMAIL', 'admin@admin.com'),
+    'default_password' => env('APP_DEFAULT_USER_PASSWORD', 'password'),
 ];
 
